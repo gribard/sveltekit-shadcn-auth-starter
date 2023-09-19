f7335752fdadd28d516ec9bdc173b8f7d1447aea
Grisha Bardiur <grisha.bardiur@gmail.com>
2023-09-19 23:44:30 +0200

    remove testprofile page

--- src/routes/(protected)/testprofile/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import type { PageData } from "./$types";
-	import SettingsForm from "./settings-form.svelte";
-	export let data: PageData;
-</script>
-
-<SettingsForm form={data.form} />
